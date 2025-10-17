0ccced3a5db22eb5f225ab906daf381bc3fdd74b
BernardoPimentel7249 <berppimentel@gmail.com>
2025-10-17 20:39:32 -0300

    correção: teste antigo era teste 3

--- tests/testa_backup.cpp
+++ tests/testa_backup.cpp
@@ -5,15 +5,8 @@
 #define CATCH_CONFIG_MAIN
 #include "../third_party/catch/catch_amalgamated.hpp"
 #include "../src/backup.hpp"
-#include <stdio.h>
-#include <unistd.h>
 
 TEST_CASE("Coluna 1: arquivo Backup.parm inexistente gera erro", "[backup]") {
-    int resultado = Salvar("tests/fixtures/nao_existe.parm", "tests/pendrive_simulado");
+    int resultado = Salvar("tests/fixtures/nao_existe.parm", "tests/pendrive_sim");
     REQUIRE(resultado != 0);
-}
-
-TEST_CASE("Coluna 2: backup.parm existe, arquivo está no HD, arquivo não está no pendrive", "[backup]") {
-    int resultado = Salvar("tests/fixtures/backup.parm", "tests/pendrive_simulado");
-    REQUIRE(resultado == 0);
 }
