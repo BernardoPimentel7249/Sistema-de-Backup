2ec2309e562c3e6a6a27a002a2fa905cdc9a5d19
BernardoPimentel7249 <berppimentel@gmail.com>
2025-10-17 21:07:06 -0300

    TESTE 4: aprovado no teste de existência do backup.parm, existência do arquivo no HD e arquivo não existente no pendrive

--- src/backup.cpp
+++ src/backup.cpp
@@ -18,15 +18,21 @@ int Salvar(const char *caminho_backup_parm, const char *caminho_destino) {
     assert(caminho_backup_parm != nullptr);
     assert(caminho_destino != nullptr);
 
-    FILE *arquivo = fopen(caminho_backup_parm, "r");
-    if (arquivo == NULL) {
+    FILE *backup_parm = fopen(caminho_backup_parm, "r");
+    if (backup_parm == NULL) {
         return 1; // erro: arquivo não existe
+    }
+
+    FILE * arquivo_destino = fopen(caminho_destino, "r");
+    if (arquivo_destino == NULL) {
+        return 1;
     }
 
     // Aqui você pode fazer o que for necessário com o arquivo aberto.
     // Exemplo: copiar para outro local (caminho_destino), etc.
 
-    fclose(arquivo);
+    fclose(arquivo_destino);
+    fclose(backup_parm);
     return 0; // sucesso
 }
 
@@ -39,6 +45,24 @@ int Salvar(const char *caminho_backup_parm, const char *caminho_destino) {
  *  Descrever o que a funcao faz
  */ 
 
-int Restaurar(char *caminho_backup_parm, char *caminho_fonte) {
-	return 0; /*!< retorna zero para teste */ 
+int Restaurar(const char *caminho_backup_parm, const char *caminho_fonte) {
+    assert(caminho_backup_parm != nullptr);
+    assert(caminho_fonte != nullptr);
+
+    FILE *backup_parm = fopen(caminho_backup_parm, "r");
+    if (backup_parm == NULL) {
+        return 1; // erro: arquivo não existe
+    }
+
+    FILE * arquivo_fonte = fopen(caminho_fonte, "r");
+    if (arquivo_fonte == NULL) {
+        return 1;
+    }
+
+    // Aqui você pode fazer o que for necessário com o arquivo aberto.
+    // Exemplo: copiar para outro local (caminho_destino), etc.
+
+    fclose(arquivo_fonte);
+    fclose(backup_parm);
+    return 0; // sucesso
 }
--- tests/testa_backup.cpp
+++ tests/testa_backup.cpp
@@ -5,8 +5,21 @@
 #define CATCH_CONFIG_MAIN
 #include "../third_party/catch/catch_amalgamated.hpp"
 #include "../src/backup.hpp"
+#include <stdio.h>
+#include <unistd.h>
 
 TEST_CASE("Coluna 1: arquivo Backup.parm inexistente gera erro", "[backup]") {
-    int resultado = Salvar("tests/fixtures/nao_existe.parm", "tests/pendrive_sim");
+    int resultado = Salvar("tests/fixtures/nao_existe.parm", "tests/fixtures/pendrive_simulado");
     REQUIRE(resultado != 0);
+}
+
+TEST_CASE("Coluna 2: backup.parm existe, arquivo está no HD, arquivo não está no pendrive", "[backup]") {
+    // Backup.parm existe e o arquivo está no HD:
+    int resultado1 = Salvar("tests/fixtures/backup.parm", "tests/fixtures/HD_simulado/backup_parm_teste4.txt");
+    REQUIRE(resultado1 == 0);
+
+    // Arquivo não está no pendrive:
+    int resultado2 = Restaurar("tests/fixtures/backup.parm", "tests/fixtures/pendrive_simulado/backup_parm_teste4.txt");
+    REQUIRE(resultado2 != 0);
+
 }
