af768718c5569ca8e563c24a4641a6d630f17a1c
BernardoPimentel7249 <berppimentel@gmail.com>
2025-10-18 17:36:39 -0300

    refatoração

--- tests/testa_backup.cpp
+++ tests/testa_backup.cpp
@@ -16,7 +16,7 @@ TEST_CASE("Coluna 1: arquivo Backup.parm inexistente gera erro", "[backup]") {
 TEST_CASE("Coluna 2: backup.parm existe, arquivo está no HD, arquivo não está no pendrive", "[backup]") {
     // Arquivo não está no pendrive:
     int resultado2 = Restaurar("tests/fixtures/backup.parm", "tests/fixtures/pendrive_simulado/backup_parm_teste4.txt");
-    REQUIRE(resultado2 != 0);
+    REQUIRE(resultado2 != 0); // nenhuma ação feita
 
     // Backup.parm existe e o arquivo está no HD:
     int resultado1 = Salvar("tests/fixtures/backup.parm", "tests/fixtures/HD_simulado/backup_parm_teste4.txt");
